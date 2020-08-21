7888a3303194706bd477b7e7d5f241c504c73b64
Piotr Kiliński <thyss@tlen.pl>
2020-08-21 06:43:58 +0200

    part of movement

---
+++ broKnight/Assets/Scripts/Components/CharacterAbilities.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAbilities : MonoBehaviour
+{
+    protected float HorizontalInput;
+    protected float VerticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    protected virtual void Update()
+    {
+        HandleAbility();
+    }
+
+    // main method, here we put the logic of ability
+    protected virtual void HandleAbility()
+    {
+        InternalInput();
+    }
+
+    //here we get neccessary input we need to perform our actions
+    protected virtual void HandleInput()
+    {
+
+    }
+
+    //here we get the main input we need to control our character
+    protected virtual void InternalInput()
+    {
+        HorizontalInput = Input.GetAxis("Horizontal");
+        VerticalInput = Input.GetAxis("Vertical");
+    }
+}
---
+++ broKnight/Assets/Scripts/Core/CharacterController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private float speed = 20f;
+
+    private Rigidbody2D myRigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        //myRigidbody2D.MovePosition(myRigidbody2D.position + movment * speed * Time.fixedDeltaTime);
+    }
+}
