014cd3a4aa9a525b48be46c4ea0e9beee92fd165
Piotr Kiliński <thyss@tlen.pl>
2020-08-22 07:44:24 +0200

    start dashing

---
+++ broKnight/Assets/Scripts/Components/CharacterDash.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterDash : CharacterAbilities
+{
+    [SerializeField] private float dashDistance = 5f;
+    [SerializeField] private float dashDuration = 0.5f;
+
+    private bool isDashing;
+    private float dashTimer;
+    private Vector2 dashOrigin;
+    private Vector2 dashDestination;
+    private Vector2 newPosition;
+
+    protected override void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Dash();
+        }
+    }
+
+    protected override void HandleAbility()
+    {
+        base.HandleAbility();
+
+        if (isDashing)
+        {
+            if (dashTimer < dashDuration)
+            {
+                newPosition = Vector2.Lerp(dashOrigin, dashDestination, dashTimer/dashDuration);
+                
+            }
+            else
+            {
+                StopDash();
+            }
+        }
+    }
+
+    private void Dash()
+    {
+        isDashing = true;
+        dashTimer = 0f;
+        dashOrigin = transform.position;
+    }
+
+    private void StopDash()
+    {
+
+    }
+}
+
