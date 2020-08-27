cad5c5b1df007508e3e8592b4c3b59ae5c77335e
Piotr Kiliński <thyss@tlen.pl>
2020-08-27 21:22:38 +0200

    player character

---
+++ broKnight/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        transform.position += new Vector3(moveInput.x *Time.deltaTime*moveSpeed, moveInput.y*Time.deltaTime* moveSpeed, 0f);
+    }
+}
